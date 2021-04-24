ea12ff7cc83567194db78c84e9ad9562f8a00a11
atichart4235 <chayanon.bunprasert@mail.kmutt.ac.th>
2021-04-24 15:58:47 +0700

    ข้อ4

---
+++ ข้อ_4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y, dx, dy, step;
+            Console.Write("input X1:");
+            float x1 = float.Parse(Console.ReadLine());
+            Console.Write("input Y1:");
+            float y1 = float.Parse(Console.ReadLine());
+            Console.Write("input X2:");
+            float x2 = float.Parse(Console.ReadLine());
+            Console.Write("input Y2:");
+            float y2 = float.Parse(Console.ReadLine());
+            dx = x2 - x1;
+            dy = y2 - y1;
+        }
+    }
+}
